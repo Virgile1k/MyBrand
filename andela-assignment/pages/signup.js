1cc9c952015eb911b2997cc034762131b0aa8c94
Vg1k <ndayambajevg16bussiness@gmail.com>
2023-02-15 15:37:57 +0200

    added code to clear form r after signup and code to correct errors in js login

--- andela-assignment/pages/signup.js
+++ andela-assignment/pages/signup.js
@@ -1,57 +1,13 @@
- const form = document.querySelector('#form-2');
-const username = document.querySelector('#username');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const password2 = document.querySelector('#password2');
-
-//Storing a user
-let currentUser = null;
-
-//Getting already eisting users from the local storage
-const users = JSON.parse(localStorage.getItem("users")) ?? [];
-let isGenuine = localStorage.getItem("isGenuine", "true"); 
-
-let id = users.length;
-
-//Function to add New Users 
-function addOfData(e) { 
-    const users = JSON.parse(localStorage.getItem("users")) ?? [{
-        id: 0,
-        username: "Ndayambaje",
-        email: "ndayambajevg16@gmail.com",
-        password: "planet@virus",
-        password2: "planet@virus"
-    }]
-    let isGenuine = localStorage.getItem("isGenuine", "true");
-    let id = users.length;
-
-    currentUser = {
-        id: id,
-        username: username.value,
-        email: email.value,
-        password: password.value,
-        password2: password2.value
-    }
-    users.push(currentUser)
-    localStorage.setItem('users', JSON.stringify(users));
-    localStorage.getItem("isGenuine", "true");
-
-    if(users.id === 0 && users.username === "Ndayambaje" && users.email === "ndayambajevg16@gmail.com" && users.password === "planet@virus" && users.password2 === "planet@virus"){
-         location.href = "../Admin-Panel/admin.html";
-        location.reload();
-    } else {
-        location.href = "../Index/index.html#contact-me-page";
-    }
-    
-}
-
-//Form Validation
-
+  const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
-    validateInputs() && addOfData();
+    validateInputs();
 });
 
 const setError = (element, message) => {
@@ -112,12 +68,14 @@ const validateInputs = () => {
     } else {
         setSuccess(password2);
     }
-
-    if (usernameValue && emailValue && passwordValue && password2Value) {
-        return true;
-         //localStorage.setItem(usernameValue, emailValue, passwordValue);
-         //location.reload();
-      } else {
-        return false;
-      }}
-      
+   if (usernameValue && emailValue && passwordValue && password2Value && passwordValue === password2Value) {
+        const user = {
+            username: usernameValue,
+            email: emailValue,
+            password: passwordValue
+        };
+        localStorage.setItem('user', JSON.stringify(user));
+        console.log('User saved to local storage:', user);
+         form.reset();
+    }
+};
