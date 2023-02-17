098dd54aa1eb11b0a8c03c475c882105fbb9e0cd
Vg1k <ndayambajevg16bussiness@gmail.com>
2023-02-17 17:22:01 +0200

    save it

--- test3.js
+++ test3.js
@@ -1,9 +1,12 @@
-const form = document.getElementById('form');
+ const form = document.getElementById('form');
 const title = document.getElementById('title');
 const date = document.getElementById('date');
 const body = document.getElementById('body');
+const picture = document.getElementById('picture');
 const category = document.getElementById('category');
- 
+const status = document.getElementById('status');
+const addBlog = document.getElementById('addBlog');
+
 form.addEventListener('submit', e => {
     e.preventDefault();
 
@@ -12,15 +15,12 @@ form.addEventListener('submit', e => {
 
 const setError = (element, message) => {
     const inputControl = element.parentElement;
-    console.log(inputControl + "input control")
     const errorDisplay = inputControl.querySelector('.error');
-    console.log(errorDisplay + "error display");
 
     errorDisplay.innerText = message;
     inputControl.classList.add('error');
-    console.log(inputControl.classList.add('error'))
-    inputControl.classList.remove('success')
-}
+    inputControl.classList.remove('success');
+};
 
 const setSuccess = element => {
     const inputControl = element.parentElement;
@@ -31,17 +31,16 @@ const setSuccess = element => {
     inputControl.classList.remove('error');
 };
 
-
 const validateInputs = () => {
     const titleValue = title.value.trim();
     const dateValue = date.value.trim();
     const bodyValue = body.value.trim();
     const pictureValue = picture.files;
     const categoryValue = category.value.trim();
-    const statusValue = statuse.value.trim();
+    const statusValue = status.value.trim();
 
-    if(titleValue === '') {
-        setError(title, 'title is required');
+    if (titleValue === '') {
+        setError(title, 'Title is required');
     } else {
         const pattern = /^[a-zA-Z]+(\s[a-zA-Z]+)*$/;
     
@@ -52,14 +51,14 @@ const validateInputs = () => {
         }
     }
 
-    if(dateValue === '') {
-        setError(date, 'date is required');
+    if (dateValue === '') {
+        setError(date, 'Date is required');
     } else {
         setSuccess(date);
     }
 
-    if(pictureValue.length === 0) {
-        setError( picture, 'image is required');
+    if (pictureValue.length === 0) {
+        setError(picture, 'Image is required');
     } else {
         const file = pictureValue[0];
         const fileType = file.type;
@@ -68,48 +67,54 @@ const validateInputs = () => {
         if (fileType !== 'image/jpeg' && fileType !== 'image/png' && fileType !== 'image/jpg') {
             setError(picture, 'File type should be jpeg, png, or jpg');
         }  else {
-            setSuccess( picture);
+            setSuccess(picture);
         }
     }
     
 
-    if(bodyValue === '') {
+    if (bodyValue === '') {
         setError(body, 'Body is required');
     } else {
         setSuccess(body);
     }
-    if(categoryValue === '') {
+
+    if (categoryValue === '') {
         setError(category, 'Category is required');
     } else {
         setSuccess(category);
     }
-    if(statusValue === '') {
-        setError(statuse, 'Status is required');
-    } else {
-        setSuccess(statuse);
-    }
 
+    if (statusValue === '') {
+        setError(status, 'Status is required');
+    } else {
+        setSuccess(status);
+    }
 };
 
+let renderImage;
+document.querySelector("#picture").addEventListener("change", function(){
+    const reader = new FileReader();
+    reader.addEventListener('load', () =>{
+        localStorage.setItem("recent-image", reader.result);
+    });
 
+    reader.readAsDataURL(this.files[0]);
+});
 
-// adding blog using local storage
-// 
-
-blogs = JSON.parse(localStorage.getItem('blogs')) || [];
+let blogs = JSON.parse(localStorage.getItem('blogs')) || [];
 
 function saveBlog() {
-
+    renderImage = localStorage.getItem('recent-image');
     validateInputs();
 
     const inputControls = form.querySelectorAll('.form-group');
     const hasError = [...inputControls].some((inputControl) => inputControl.classList.contains('error'));
   
-    if (hasError) {
-      return;
+    if (hasError) {     return;
     }
 
   let blog = {};
+  blog.picture = renderImage
   blog.title = title.value
   blog.date = date.value
   blog.body= body.value
@@ -122,42 +127,4 @@ function saveBlog() {
    window.location.replace("article.html");
 }
 
-// function displayBlogs() {
-  
-//     // retrieve the form data from local storage
-//     const blogDataJson = localStorage.getItem('blogs');
-
-//     // parse the JSON string to an object
-//     const blogData = JSON.parse(blogDataJson);
-
-//     const blogsTable = document.querySelector('.board tbody');
-//     blogsTable.innerHTML = '';
-//     blogs.forEach((blog, index) => {
-//       const tr = document.createElement('tr');
-//       tr.innerHTML = `
-//         <td class="picture">
-//           <img src="${blogData.cover}" alt="">
-//         </td>
-//         <td class="blog-title">
-//           <p>${blogData.title}</p> 
-//         </td>
-//         <td class="author">
-//           <p>${blogData.author}</p>
-//         </td>
-//         <td class="date">
-//           <p>${blogData.date}</p>
-//         </td>
-//         <td class="status">
-//           <p>${blogData.status}</p>
-//         </td>
-//         <td class="action">
-//           <a href="edit.html"><i class='bx bxs-edit'></i></a> 
-//           <a href=""><i class='bx bxs-trash'></i></a> 
-//         </td>
-//       `;
-//       blogsTable.appendChild(tr);
-//     });
-//   }
-  
-
-submit.onclick = saveBlog;
+addBlog.onclick = saveBlog;
