5f32d1fb2e9b35b62a99baf62501a57f2e07dd6d
Vg1k <ndayambajevg16bussiness@gmail.com>
2023-02-20 13:28:48 +0200

    edited editorial space

--- editorialspace.js
+++ editorialspace.js
@@ -1,163 +1,124 @@
-const form = document.getElementById('form');
-const title = document.getElementById('title');
-const date = document.getElementById('date');
-const body = document.getElementById('body');
-const category = document.getElementById('category');
- 
-form.addEventListener('submit', e => {
-    e.preventDefault();
+  //form validation 
+  // Validate form inputs
+function validateForm() {
+  var title = document.getElementById('title').value;
+  var date = document.getElementById('date').value;
+  var body = document.getElementById('body').value;
 
-    validateInputs();
+  // Check that all required fields are filled in
+  if (!title || !date || !body) {
+    alert('Please fill in all required fields.');
+    return false;
+  }
+
+  // Check that the date is in a valid format (yyyy-mm-dd)
+  var dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!date.match(dateRegex)) {
+    alert('Please enter the date in the format yyyy-mm-dd.');
+    return false;
+  }
+
+  return true;
+}
+
+// Save blog data to local storage and redirect to blog page
+document.getElementById('blogForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  // Validate form inputs
+  if (!validateForm()) {
+    return;
+  }
+
+  var title = document.getElementById('title').value;
+  var image = document.getElementById('imagePreview').src;
+  var date = document.getElementById('date').value;
+  var body = document.getElementById('body').value;
+
+  var blogData = {
+    title: title,
+    image: image,
+    date: date,
+    body: body
+  };
+
+  blogDataArray.push(blogData);
+  localStorage.setItem('blogDataArray', JSON.stringify(blogDataArray));
+
+  alert('Blog data saved to local storage!');
+  document.getElementById('blogForm').reset();
+  clearPreview();
 });
 
-const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    console.log(inputControl + "input control")
-    const errorDisplay = inputControl.querySelector('.error');
-    console.log(errorDisplay + "error display");
+  //form validation
+    var blogDataArray = [];
 
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    console.log(inputControl.classList.add('error'))
-    inputControl.classList.remove('success')
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-};
-
-
-const validateInputs = () => {
-    const titleValue = title.value.trim();
-    const dateValue = date.value.trim();
-    const bodyValue = body.value.trim();
-    const pictureValue = picture.files;
-    const categoryValue = category.value.trim();
-    const statusValue = statuse.value.trim();
-
-    if(titleValue === '') {
-        setError(title, 'title is required');
-    } else {
-        const pattern = /^[a-zA-Z]+(\s[a-zA-Z]+)*$/;
-    
-        if (!pattern.test(titleValue)) {
-            setError(title, 'Title should contain only characters');
-        } else {
-            setSuccess(title);
-        }
+    // Preview uploaded image
+    function previewImage(event) {
+        var image = document.getElementById('imagePreview');
+        image.src = URL.createObjectURL(event.target.files[0]);
     }
 
-    if(dateValue === '') {
-        setError(date, 'date is required');
-    } else {
-        setSuccess(date);
+    // Preview blog
+    function previewBlog() {
+        var title = document.getElementById('title').value;
+        var image = document.getElementById('imagePreview').src;
+        var date = document.getElementById('date').value;
+        var body = document.getElementById('body').value;
+        var blogPreview = '<div class="blog-card">' +
+            '<h2>' + title + '</h2>' +
+            '<img src="' + image + '" alt="">' +
+            '<p>Date: ' + date + '</p>' +
+            '<p>' + body + '</p>' +
+            '</div>';
+        document.getElementById('blogPreview').innerHTML = blogPreview;
     }
 
-    if(pictureValue.length === 0) {
-        setError( picture, 'image is required');
-    } else {
-        const file = pictureValue[0];
-        const fileType = file.type;
-        const fileSize = file.size;
-    
-        if (fileType !== 'image/jpeg' && fileType !== 'image/png' && fileType !== 'image/jpg') {
-            setError(picture, 'File type should be jpeg, png, or jpg');
-        }  else {
-            setSuccess( picture);
-        }
-    }
-    
+    // Save blog data to local storage and redirect to blog page
+    document.getElementById('blogForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var title = document.getElementById('title').value;
+        var image = document.getElementById('imagePreview').src;
+        var date = document.getElementById('date').value;
+        var body = document.getElementById('body').value;
 
-    if(bodyValue === '') {
-        setError(body, 'Body is required');
-    } else {
-        setSuccess(body);
-    }
-    if(categoryValue === '') {
-        setError(category, 'Category is required');
-    } else {
-        setSuccess(category);
-    }
-    if(statusValue === '') {
-        setError(statuse, 'Status is required');
-    } else {
-        setSuccess(statuse);
+        var blogData = {
+            title: title,
+            image: image,
+            date: date,
+            body: body
+        };
+
+        blogDataArray.push(blogData);
+        localStorage.setItem('blogDataArray', JSON.stringify(blogDataArray));
+
+        alert('Blog data saved to local storage!');
+        document.getElementById('blogForm').reset();
+        clearPreview();
+    });
+
+    // Clear preview
+    function clearPreview() {
+        document.getElementById('blogPreview').innerHTML = '';
+        document.getElementById('imagePreview').src = '#';
     }
 
-};
+    // Load blog data from local storage
+    var storedBlogDataArray = JSON.parse(localStorage.getItem('blogDataArray'));
+    if (storedBlogDataArray) {
+        blogDataArray = storedBlogDataArray;
+        for (var i = 0; i < blogDataArray.length; i++) {
+            var blogData = blogDataArray[i];
+            var title = blogData.title;
+            var image = blogData.image;
+            var date = blogData.date;
+            var body = blogData.body;
 
-
-
-// adding blog using local storage
-// 
-
-blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-
-function saveBlog() {
-
-    validateInputs();
-
-    const inputControls = form.querySelectorAll('.form-group');
-    const hasError = [...inputControls].some((inputControl) => inputControl.classList.contains('error'));
-  
-    if (hasError) {
-      return;
-    }
-
-  let blog = {};
-  blog.title = title.value
-  blog.date = date.value
-  blog.body= body.value
-  blog.category = category.value
-  blog.statuse = statuse.value
-  blogs.push(blog);
-  const stringBlogs = JSON.stringify(blogs);
-  localStorage.setItem('blogs', stringBlogs);
-    // Redirect to login page after saving blog
-   window.location.replace("article.html");
-}
-
-// function displayBlogs() {
-  
-//     // retrieve the form data from local storage
-//     const blogDataJson = localStorage.getItem('blogs');
-
-//     // parse the JSON string to an object
-//     const blogData = JSON.parse(blogDataJson);
-
-//     const blogsTable = document.querySelector('.board tbody');
-//     blogsTable.innerHTML = '';
-//     blogs.forEach((blog, index) => {
-//       const tr = document.createElement('tr');
-//       tr.innerHTML = `
-//         <td class="picture">
-//           <img src="${blogData.cover}" alt="">
-//         </td>
-//         <td class="blog-title">
-//           <p>${blogData.title}</p> 
-//         </td>
-//         <td class="author">
-//           <p>${blogData.author}</p>
-//         </td>
-//         <td class="date">
-//           <p>${blogData.date}</p>
-//         </td>
-//         <td class="status">
-//           <p>${blogData.status}</p>
-//         </td>
-//         <td class="action">
-//           <a href="edit.html"><i class='bx bxs-edit'></i></a> 
-//           <a href=""><i class='bx bxs-trash'></i></a> 
-//         </td>
-//       `;
-//       blogsTable.appendChild(tr);
-//     });
-//   }
-  
-
-submit.onclick = saveBlog;
+            var blogPreview = '<div class="blog-card">' +
+                '<h2>' + title + '</h2>' +
+                '<img src="' + image + '" alt="">' +
+                '<p>Date: ' + date + '</p>' +
+                '<p>' + body + '</p>' +
+                '</div>';
+            document.getElementById('blogPreview').innerHTML=blogPreview;
+        }}
